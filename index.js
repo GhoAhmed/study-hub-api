0b0252f055dd0d09fcf385e88bb713b15f504165
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2025-12-28 21:06:29 +0100

    authentication - login & register

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const connectDB = require("./db");
+const authRoutes = require("./routes/authRoute");
 
 dotenv.config();
 connectDB();
+
+app.use(express.json());
+
+// User routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
