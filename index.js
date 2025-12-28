f165b8326a997e312021f67be8a1ea5b0d4f00eb
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2025-12-28 17:58:34 +0100

    config

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // this is my server file
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
